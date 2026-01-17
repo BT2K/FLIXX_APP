688061cdf9d36ec22b921b46e0da94e62b84bc05
n3wbie <bartlomiej.cok@icloud.com>
2026-01-17 20:26:05 +0100

    ADD API MOVIES LIST

--- js/script.js
+++ js/script.js
@@ -1,1 +1,93 @@
 "use strict";
+
+//Check for curent page
+const globalState = {
+  currentPage: window.location.pathname,
+};
+
+//Popular Movies from API
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+
+          ${
+            movie.poster_path
+              ? `<img
+              src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+              : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+          }
+
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+    `;
+    document.getElementById("popular-movies").appendChild(div);
+  });
+}
+
+// TV Shows from API
+async function displayTVShows() {}
+
+// Fetch Data from TMDB API
+async function fetchAPIData(endpoint) {
+  //I know that's a big no no when it comes to commercial aps ;)
+  const API_KEY = "4cd41e964ccf6d5c0606124a17235e0e";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.json();
+
+  return data;
+}
+
+//Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((link) => {
+    if (link.getAttribute("href") === globalState.currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
+//Init App
+function init() {
+  switch (globalState.currentPage) {
+    case "/":
+    case "/index.html":
+      displayPopularMovies();
+      break;
+    case "/shows.html":
+      displayTVShows();
+      break;
+    case "/movie-details.html":
+      console.log("Movie Details");
+      break;
+    case "/tv-details.html":
+      console.log("TV Details");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+  }
+  highlightActiveLink();
+}
+
+document.addEventListener("DOMContentLoaded", init);
